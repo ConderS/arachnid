fa020348f9ae79529c9d51685110923cebb456a1
Conder <cs3544@columbia.edu>
2018-03-08 02:39:49 -0500

    dragging works but bar is behind everything and mouseup not working

--- interface/src/components/BarChart.js
+++ interface/src/components/BarChart.js
@@ -13,7 +13,17 @@ import '../styles/components/barchart.css';
 class BarChart extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            currentDatum: [],
+            mouseover: false,
+            dragStart: false
+        }
         this.createBarChart = this.createBarChart.bind(this);
+        this.handleMouseOut = this.handleMouseOut.bind(this);
+        this.handleMouseOver = this.handleMouseOver.bind(this);
+        this.handleDragEnd = this.handleDragEnd.bind(this);
+        this.handleDragStart = this.handleDragStart.bind(this);
     }
 
     componentDidMount() {
@@ -21,7 +31,17 @@ class BarChart extends Component {
     }
 
     componentDidUpdate() {
+        console.log("UPDATE: ");
         this.createBarChart();
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        const { mouseover, currentDatum, dragStart } = this.state;
+
+        if (mouseover !== nextState.mouseover || currentDatum || !currentDatum || dragStart !== nextState.dragStart) {
+            return false;
+        }
+        return true;
     }
 
     createBarChart() {
@@ -29,7 +49,7 @@ class BarChart extends Component {
 
         const node = this.node;
         const dataMax = max(chartData.map(d => mean(d.review_count)));
-        const barWidth = size[0] / chartData.length;
+        const barWidth = size[0] / chartData.length + 10;
         const yScale = scaleLinear().domain([0, dataMax]).range([0, size[1]]);
 
 
@@ -44,7 +64,10 @@ class BarChart extends Component {
         select(node).append('svg').append('g').attr('id', 'BC-yAxis').call(yAxis);
         //
         
-        var dragAction = drag().on('drag', this.move);
+        var dragAction = drag()
+                            .on('start', this.handleDragStart)
+                            .on('drag', this.handleDragging)
+                            .on('end', this.handleDragEnd)
 
         select(node)
             .selectAll('rect.bc-bar')
@@ -52,7 +75,6 @@ class BarChart extends Component {
             .enter()
             .append('rect')
                 .attr('class', 'bc-bar')
-                //add .on drag
         
         select(node)
             .selectAll('rect.bc-bar')
@@ -63,24 +85,86 @@ class BarChart extends Component {
         select(node)
             .selectAll('rect.bc-bar')
             .data(chartData)
-                .attr('x', (d, i) => i * (barWidth + 5))
+                .attr('x', (d, i) => i * (barWidth + 20))
                 .attr('y', d => size[1] - yScale(mean(d.review_count)))
                 .attr('height', d => yScale(mean(d.review_count)))
                 .attr('width', barWidth)
                 .style('fill', (d, i) => 'blue')
                 .style('stroke', 'black')
+                .on('mouseover', this.handleMouseOver)
+                .on('mouseout', this.handleMouseOut)
+                .on('mousedown', this.handleDown)
+                .on('mouseup', this.handleUp)
                 .call(dragAction);
+
     }
 
-    move(d) {
+    handleDragStart(d) {
+        this.setState({ currentDatum: [d], dragStart: true });
+    }
+
+    handleDragging(d) {
         var bar = select(this);
 
-        console.log("EVENT: ", currentEvent);
+        bar.lower();
         //Update position of bar by adding drag distance to each coordinate
         bar.attr('x', d.x = currentEvent.x)
-            .attr('y', d.y = currentEvent.y);
-        console.log('e: ', currentEvent.x, currentEvent.y);
+            .attr('y', d.y = currentEvent.y)
+            .classed('bc-active-bar', true);
 
+
+    }
+
+    handleDown(d) {
+        var bar = select(this);
+
+        bar.style('fill', 'red');
+    }
+
+    handleUp(d) {
+        console.log('up');
+        var bar = select(this);
+
+        bar.style('fill', 'blue');
+    }
+
+
+    handleDragEnd(d) {
+        const { mouseover, currentDatum } = this.state;
+
+        if (mouseover && currentDatum.length > 1) {
+            console.log("DEDUP");
+
+            //Do Dedup and send in currentDatum
+        }
+
+        this.setState({ currentDatum: [] });
+    }
+
+    handleMouseOver(d) {
+        const { currentDatum } = this.state;
+
+        // if (currentDatum.length == 0) { return; }
+
+        if (currentDatum.length > 0 && d.business_id !== currentDatum[0].business_id) {
+            console.log("Hovering Different Element");
+            var newDatum = currentDatum;
+            newDatum.push(d);
+            this.setState({ mouseover: true, currentDatum: newDatum });
+        }
+    }
+
+    handleMouseOut(d) {
+        const { currentDatum } = this.state;
+
+        // if (currentDatum.length == 0) { return; }
+
+        if (currentDatum.length > 0 && d.business_id !== currentDatum[0].business_id) {
+            console.log("Mouse Out on Diff Element");
+            var newDatum = currentDatum;
+            newDatum.pop(d);
+            this.setState({ mouseover: false, currentDatum: newDatum });
+        }
     }
 
     render() {
