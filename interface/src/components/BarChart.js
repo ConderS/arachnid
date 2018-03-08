ce0d170e784a70409c255f4e29d6842e99ca01d8
Conder <cs3544@columbia.edu>
2018-03-07 22:35:39 -0500

    styling and axis kinda

--- interface/src/components/BarChart.js
+++ interface/src/components/BarChart.js
@@ -4,6 +4,7 @@ import { max, sum, mean } from 'd3-array';
 import { select } from 'd3-selection';
 import { legendColor } from 'd3-svg-legend';
 import { transition } from 'd3-transition';
+import { axisBottom, axisRight} from 'd3-axis';
 
 import '../styles/components/barchart.css';
 
@@ -28,7 +29,15 @@ class BarChart extends Component {
         const dataMax = max(chartData.map(d => mean(d.review_count)));
         const barWidth = size[0] / chartData.length;
 
-        const yScale = scaleLinear().domain([0, dataMax]).range([0, size[1]])
+        const xScale = scaleLinear().domain([0, chartData.length]);
+        const yScale = scaleLinear().domain([0, dataMax]).range([0, size[1]]);
+
+        var xAxis = axisBottom().scale(xScale);
+        var yAxis = axisRight().scale(yScale);
+
+        select(node).append('svg').append('g').attr('id', 'BC-xAxis').attr('transform', 'translate(0, ' + size[1] + ')').call(xAxis);
+
+        select(node).append('svg').append('g').attr('id', 'BC-yAxis').call(yAxis);
 
         select(node)
             .selectAll('rect.bc-bar')
@@ -47,7 +56,7 @@ class BarChart extends Component {
         select(node)
             .selectAll('rect.bc-bar')
             .data(chartData)
-                .attr('x', (d, i) => i * barWidth + 50)
+                .attr('x', (d, i) => i * (barWidth + 5))
                 .attr('y', d => size[1] - yScale(mean(d.review_count)))
                 .attr('height', d => yScale(mean(d.review_count)))
                 .attr('width', barWidth)
