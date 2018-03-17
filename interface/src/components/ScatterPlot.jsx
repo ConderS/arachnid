8af90f5beede364dc399b5cf2b41eaba7fcb9055
Conder <cs3544@columbia.edu>
2018-03-17 12:18:37 -0400

    added comments to explain some react in scatterplot

--- interface/src/components/ScatterPlot.jsx
+++ interface/src/components/ScatterPlot.jsx
@@ -10,7 +10,10 @@ export class ScatterPlot extends Component {
             spaceOffset: 300
         }
 
+        //Binding your functions sets the "this" context to that of the class, not the function itself (so you can call "this.state" and "this._OTHER_FUNCTION")
+        // Whenever you create a new function in the component and you want it to be able to refer to outside functions and the state/props, you bind it like so here.
         this.createScatterPlot = this.createScatterPlot.bind(this);
+        this.someEventHandler = this.someEventHandler.bind(this);
     }
     
     componentDidMount() {
@@ -34,6 +37,23 @@ export class ScatterPlot extends Component {
         
         // WRITE HERE
         // The rest of the D3 code goes here, just select(node) to select the svg
+    }
+
+    someEventHandler() {
+
+        /* Props is a global state.
+        
+         updateCurrentDatum = a function that stores an ARRAY of selected data, or in this case, yelp records. When called, does not reset the viz. 
+        
+         currentDatum = the current array of selected data/yelp records
+
+         currentDatum will be empty if you haven't updated it with "updateCurrentDatum" previously.
+
+         updateData = updates the data of the viz. Will RESET the viz with the new data
+        */
+       
+        const { updateData, updateCurrentDatum, currentDatum } = this.props;
+
     }
 
     render() {
