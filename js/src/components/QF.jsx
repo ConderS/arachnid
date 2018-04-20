324f3ac3eba642928cc6a42ae1c236acc08a842c
Conder <cs3544@columbia.edu>
2018-04-20 03:15:05 -0400

    qf components

---
+++ js/src/components/QF.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../styles/components/qf.css';
+
+export class QF extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div></div>
+            );
+    }
+}
+
+export default QF;
--- js/src/components/Specification.jsx
+++ js/src/components/Specification.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 import '../styles/components/specification.css';
 
+import QF from './QF';
+
 // Gives interactive list of quality functions
 export class Specification extends Component {
     constructor(props) {
         super(props);
+
+        this.renderQFList = this.renderQFList.bind(this);
+    }
+
+    renderQFList() {
+        return (
+            <QF />
+            );
     }
 
     render() {
@@ -12,7 +22,7 @@ export class Specification extends Component {
             <div className="sp-container">
                 <h3 className="menu-header">Quality Functions</h3>
                 <hr/>
-                
+                {this.renderQFList()}
             </div>
             );
     }
